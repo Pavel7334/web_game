1c088829f745d2d1ed6343adc20348f64a504333
Pavel7334 <novaya.rabota.pavla2016@yandex.ru>
2024-05-15 00:29:34 +0400

    Добавил 2 модели Character + логика, UserCharacter

--- app/users/models.py
+++ app/users/models.py
@@ -7,7 +7,13 @@ from app.database import Base
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, nullable=False)
+    id = Column(Integer, primary_key=True, index=True, doc="Уникальный идентификатор пользователя")
+    email = Column(String, unique=True, nullable=False, doc="Email пользователя")
     username = Column(String, unique=True, index=True, nullable=False, doc="Имя пользователя")
     password = Column(String, nullable=False, doc="Хэшированный пароль пользователя")
+
+    characters = relationship(
+        "Character",
+        secondary="user_characters",
+        back_populates="users",
+        doc="Персонажи, принадлежащие пользователю")
--- migrations/env.py
+++ migrations/env.py
@@ -12,6 +12,8 @@ sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
 from app.config import settings
 from app.database import Base
 from app.users.models import User
+from app.characters.models import Character
+from app.characters.models import UserCharacter
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
